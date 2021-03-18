3d3b97654b0ace76d79918d042f471ab6a8cd0d0
KJM-PC\wjdan <wjdanr89@gmail.com>
2021-03-19 08:59:27 +0900

    design-pattern: 챕터 7

---
+++ src/main/java/design/pattern/chapter7/Adapter.java
@@ -0,0 +1,6 @@
+package design.pattern.chapter7;
+
+public interface Adapter {
+	public int twiceOf(int num);
+	public int halfOf(int num);
+}
---
+++ src/main/java/design/pattern/chapter7/Math.java
@@ -0,0 +1,11 @@
+package design.pattern.chapter7;
+
+public class Math {
+	public float twoTime(float num) {
+		return num * 2;
+	}
+
+	public float halfTime(float num) {
+		return num / 2;
+	}
+}
